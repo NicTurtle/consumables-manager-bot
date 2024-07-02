60c53bce20fc518874e1d10031c0890887c7c422
NicTurtle <nic.turtle1012@gmail.com>
2024-07-02 03:15:35 +0300

    connected to the database

--- src/main/java/com/github/nicturtle/model/repository/WickRepository.java
+++ src/main/java/com/github/nicturtle/model/repository/WickRepository.java
@@ -2,6 +2,8 @@ package com.github.nicturtle.model.repository;
 
 import com.github.nicturtle.model.entity.Wick;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WickRepository extends JpaRepository<Wick, Long> {
 }
